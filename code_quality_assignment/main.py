df473c627d8012e3700b78a31e6d69b3194aa3fb
Aastha Mahajan <aasthamahajan24@gmail.com>
2025-04-05 18:57:43 -0500

    Optimized expensive_op()using formula

--- code_quality_assignment/main.py
+++ code_quality_assignment/main.py
@@ -1,10 +1,7 @@
 from line_profiler import LineProfiler
-def expensive_op(n):
-    total = 0
-    for i in range(1000):
-        total += i * n
-    return total
 
+def expensive_op(n):
+    return n * (999 * 1000) // 2
 
 def slow_func(lst):
     result = []
@@ -12,19 +9,15 @@ def slow_func(lst):
         result.append(expensive_op(i))
     return result
 
-
-def unused_function():
-    x = 10
-    y = 20
-    z = x + y
-    return z
-
-
-def main():
+def profile_main():
     numbers = list(range(1000))
     output = slow_func(numbers)
     print("Done")
 
-
 if __name__ == "__main__":
-    main()
+    profiler = LineProfiler()
+    profiler.add_function(expensive_op)
+    profiler.enable()
+    profile_main()
+    profiler.disable()
+    profiler.print_stats()
